8729fff9aa8458c53a1da6dcabc4040ba2c1da0f
GabrielMurkovic <murkovicgabriel@gmail.com>
2026-06-06 18:20:14 +0200

    Add files via upload

---
+++ LV20 - zagrade u izrazu.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+bool izjednaceno(string izraz) {
+	stack <char> s;
+	int str_len = izraz.length();
+
+	for (int i = 0; i < str_len; i++) {
+	char znak = izraz[i];
+	if (znak == '(') {
+		s.push(znak);
+		}
+	else if (znak == ')') {
+		if(s.empty()) return false;
+		s.pop();
+	}
+	}
+	return s.empty();
+}
+int main() {
+	string unos;
+	getline(cin, unos);
+
+	if(izjednaceno(unos)){
+	cout << "Izjednaceno";
+	}
+	else{
+	cout << "Nije izjednaceno";}}
