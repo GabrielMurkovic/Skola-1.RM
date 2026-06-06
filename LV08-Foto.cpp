644b9b192484c44822532f38bf47725c9733391a
GabrielMurkovic <murkovicgabriel@gmail.com>
2026-06-06 19:12:54 +0200

    Add files via upload

---
+++ LV08-Foto.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int M;
+    cin >> M;
+
+    int N;
+    cin >> N;
+
+    int neuspjele = 0;
+
+    for (int i = 0; i < N; i++) {
+        int V;
+        cin >> V;
+
+        if (V <= M) {
+            M -= V;
+        }
+        else {
+            neuspjele++;
+        }
+    }
+
+    cout << neuspjele << endl;
+
+    return 0;
+}
