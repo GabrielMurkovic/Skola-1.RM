3d3d77eaeac553e328c52b19ae9a48d723fde77f
GabrielMurkovic <murkovicgabriel@gmail.com>
2026-06-06 18:19:44 +0200

    Add files via upload

---
+++ LV20  - Palindrom.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+#include <cctype>
+using namespace std;
+
+bool jepalindrom(string izraz) {
+	stack <char> s;
+	int str_len = izraz.length();
+	string filtrirano;
+
+	for (int i = 0; i < str_len; i++) {
+		char znak = izraz[i];
+		if (isalpha(znak)) {
+		filtrirano += tolower(znak);
+		}
+	}
+	for (int i = 0; i < filtrirano.length(); i++) {
+	s.push(filtrirano[i]);
+
+	}
+	for (int i = 0; i < filtrirano.length(); i++) {
+	if(s.empty()  || filtrirano[i] != s.top()){
+		
+	return false;
+
+	}
+	s.pop();
+	}
+	return true;
+}
+int main(){
+string unos;
+getline(cin,unos);
+if(jepalindrom(unos)){
+cout << "palindrom";
+}else{
+cout << "Nije palindrom ";}}
