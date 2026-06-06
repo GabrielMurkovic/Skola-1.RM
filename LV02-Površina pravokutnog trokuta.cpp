69a9e69fc19f4c5d7ec59ab218d34d62d4833cb0
GabrielMurkovic <murkovicgabriel@gmail.com>
2026-06-06 18:15:33 +0200

    Add files via upload

---
+++ LV02-Površina pravokutnog trokuta.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    
+int n,a;
+cout << "Unesi a: ";
+cin >> n;
+cout << "Unesi b: ";
+cin >> a;
+double h = (double) sqrt(pow(n,2)+pow(a,2));
+double p = (double)(n*a)/2.0;
+cout << "Hipotenuza je: " << h << endl;
+cout << "Površina je: " << p << endl;
+}
