7a8c3b8f30c868067aba811e0ee9182b892acf25
GabrielMurkovic <murkovicgabriel@gmail.com>
2026-06-06 19:12:41 +0200

    Add files via upload

---
+++ LV08-Abeceda.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for (char c = 'A'; c <= 'Z'; c++) {
+        cout << c;
+    }
+    cout << endl;
+
+    for (char c = 'a'; c <= 'z'; c++) {
+        cout << c;
+    }
+    cout << endl;
+
+    for (char c = '0'; c <= '9'; c++) {
+        cout << c;
+    }
+    cout << endl;
+
+    return 0;
+}
