27aa2679d0ac073e40038dbe313d428eb54bff55
GabrielMurkovic <murkovicgabriel@gmail.com>
2026-06-06 19:39:11 +0200

    Add files via upload

---
+++ Stack -pretvaranje dekadskog u binarni.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+string bintodec(int decbroj) {
+	stack <int> remstack;
+	string binstring ="";
+
+	while (decbroj > 0) {
+		int rem = decbroj %2;
+		remstack.push(rem);
+		decbroj = decbroj /2;
+	}
+	while (!remstack.empty()) {
+		binstring.append(to_string(remstack.top()));
+		remstack.pop();
+	}
+	return binstring;
+}
+int main() {
+	int unos;
+	cin >> unos;
+	cout << bintodec(unos);
+}
