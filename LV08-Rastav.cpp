4752976da0dbfdbe38d4084d4ba85dd19eec8d5c
GabrielMurkovic <murkovicgabriel@gmail.com>
2026-06-06 19:13:05 +0200

    Add files via upload

---
+++ LV08-Rastav.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string broj;
+    cin >> broj;
+
+    if (broj.length() != 6) {
+        cout << "Pogresan unos!" << endl;
+        return 0;
+    }
+
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << ".";
+        }
+
+        cout << broj[i];
+
+        for (int j = i + 1; j < 6; j++) {
+            cout << ".";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
