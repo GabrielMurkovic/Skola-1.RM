b8fb8f09bdad762d8ab87d30db9d39f1779744e5
GabrielMurkovic <murkovicgabriel@gmail.com>
2026-06-06 18:15:57 +0200

    Add files via upload

---
+++ LV02-Površina raznostraničnog trokuta.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+ int a,b,c;
+ cout << "Unesi a: ";
+ cin >> a;
+ cout << "Unesi b: ";
+ cin >> b;
+ cout << "Unesi c: ";
+ cin >> c;
+ if (a + b <= c || a + c <= b || b + c <= a) {
+     cout << "Nevalidan trokut!" << endl;
+     return 0;
+ }
+
+ 
+ double s =(a + b + c)/2.0;
+double P = sqrt(s*(s-a)*(s-b)*(s-c));
+cout << "Povrsina je: "<<P << endl;
+}
