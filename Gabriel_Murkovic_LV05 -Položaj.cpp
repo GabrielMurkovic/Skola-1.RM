ed9de9d3b44c0a8522dd5f5d897c6f3459f84d6a
GabrielMurkovic <murkovicgabriel@gmail.com>
2026-06-06 20:17:01 +0200

    Add files via upload

---
+++ Gabriel_Murkovic_LV05 -Položaj.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double x, y;
+    cout << "Unesi koordinate tocke (x y): ";
+    cin >> x >> y;
+
+    if (x > 0 && y > 0)
+        cout << "Prvi kvadrant" << endl;
+    else if (x < 0 && y > 0)
+        cout << "Drugi kvadrant" << endl;
+    else if (x < 0 && y < 0)
+        cout << "Treci kvadrant" << endl;
+    else if (x > 0 && y < 0)
+        cout << "Cetvrti kvadrant" << endl;
+    else if (x == 0 && y == 0)
+        cout << "Ishodiste" << endl;
+    else if (x == 0)
+        cout << "Os y" << endl;
+    else if (y == 0)
+        cout << "Os x" << endl;
+
+    return 0;
+}
