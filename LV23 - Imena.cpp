042ffabd3767a287b6a7fe85da31cf4840f52468
GabrielMurkovic <murkovicgabriel@gmail.com>
2026-06-06 18:21:39 +0200

    Add files via upload

---
+++ LV23 - Imena.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+#include <string>
+#include <queue>
+using namespace std;
+
+char prvoslovo(string Ime) {
+  return Ime[0];
+  }
+
+int main(){
+int brojimena = 0;
+queue <string> Imena;
+
+ while(true){
+ string Ime;
+ cin >> Ime;
+ if(Ime != "Kraj"){
+ Imena.push(Ime);
+ brojimena++;}
+ if(Ime == "Kraj"){
+ break;
+}}
+
+ cout << brojimena << " ";
+while(!Imena.empty()){
+string Ime = Imena.front();
+cout << prvoslovo(Ime) << ". ";
+Imena.pop();
+}
+
+}
+
