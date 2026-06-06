fed5a6a74d611ae487c28af750788ab03fe1eff4
GabrielMurkovic <murkovicgabriel@gmail.com>
2026-06-06 19:19:01 +0200

    Add files via upload

---
+++ LV07-Kalkulator.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a, b;
+    char op;
+
+    cout << "Unesite dva broja i operator: ";
+    cin >> a >> b >> op;
+
+    int rezultat;
+
+    switch (op) {
+
+    case '+':
+        rezultat = a + b;
+        break;
+
+    case '-':
+        rezultat = a - b;
+        break;
+
+    case '*':
+        rezultat = a * b;
+        break;
+
+    case '/':
+        if (b == 0) {
+            cout << "Greska: dijeljenje s nulom!" << endl;
+            return 0;
+        }
+        rezultat = a / b;
+        break;
+
+    case '%':
+        if (b == 0) {
+            cout << "Greska: modulo s nulom!" << endl;
+            return 0;
+        }
+        rezultat = a % b;
+        break;
+
+    default:
+        cout << "Greska: nepoznat operator!" << endl;
+        return 0;
+    }
+
+    cout << a << op << b << "=" << rezultat << endl;
+
+    return 0;
+}
