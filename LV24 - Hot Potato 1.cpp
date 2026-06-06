4f0af3adf9e99be631f114f7e280f15fda47f1e2
GabrielMurkovic <murkovicgabriel@gmail.com>
2026-06-06 18:23:03 +0200

    Add files via upload

---
+++ LV24 - Hot Potato 1.cpp
@@ -0,0 +1,34 @@
+﻿
+
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+int main()
+{
+ 
+ int n;
+ cout << "Unesi broj igraca: ";
+ cin >> n;
+queue <string> q;
+
+for (int i = 0; i < n; i++) {
+	string ime;
+	cin >> ime;
+	q.push(ime);
+}
+
+int k;
+cin >> k;
+while(q.size()>1){
+
+for (int i = 0; i < k; i++) {
+	q.push(q.front());
+	q.pop();}
+
+	cout << "Ispada: " << q.front() << endl;
+	q.pop();
+}
+cout << "Pobjednik je: " << q.front();
+}
