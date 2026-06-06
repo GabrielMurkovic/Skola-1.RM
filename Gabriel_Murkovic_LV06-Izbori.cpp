276e9f6150846b1f68a06d87b379193eba91ff0a
GabrielMurkovic <murkovicgabriel@gmail.com>
2026-06-06 20:21:06 +0200

    Add files via upload

---
+++ Gabriel_Murkovic_LV06-Izbori.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    int P, Q;
+    cin >> P >> Q;
+
+    // Odreðujemo dva kandidata s najviše glasova u prvom krugu
+    int prvi, drugi;
+
+    // Prvi maksimum
+    if (a > b && a > c && a > d) prvi = 0;
+    else if (b > a && b > c && b > d) prvi = 1;
+    else if (c > a && c > b && c > d) prvi = 2;
+    else prvi = 3;
+
+    // Drugi maksimum
+    if (prvi != 0 && ((a > b && a > c && a > d) || (a != glasovi[prvi]))) drugi = 0;
+    else if (prvi != 1 && ((b > a && b > c && b > d) || (b != glasovi[prvi]))) drugi = 1;
+    else if (prvi != 2 && ((c > a && c > b && c > d) || (c != glasovi[prvi]))) drugi = 2;
+    else drugi = 3;
+
+    // Odreðujemo abecedni red
+    int manji = (prvi < drugi) ? prvi : drugi;
+    int veci = (prvi > drugi) ? prvi : drugi;
+
+    // Odreðujemo pobjednika drugog kruga
+    char pobjednik = (P > Q) ? ('A' + manji) : ('A' + veci);
+
+    cout << pobjednik << endl;
+    return 0;
+}
