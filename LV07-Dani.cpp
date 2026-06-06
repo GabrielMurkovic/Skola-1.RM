35df60b4258110b9479585142b94b6c9f9aca561
GabrielMurkovic <murkovicgabriel@gmail.com>
2026-06-06 19:03:44 +0200

    Add files via upload

---
+++ LV07-Dani.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>   
+
+using namespace std;     
+
+int main() {
+
+    int dan;        
+    cout << "Unesite redni broj dana u tjednu (1-7): ";
+    cin >> dan;        
+
+    if (dan < 1 || dan > 7) {
+        cout << "Pogresan unos!" << endl;
+        return 0;        
+    }
+
+    cout << "Dani od unesenog do kraja tjedna:" << endl;
+
+    switch (dan) {
+
+    case 1:
+        cout << "PONEDJELJAK - ";
+    case 2:
+        cout << "UTORAK - ";
+    case 3:
+        cout << "SRIJEDA - ";
+    case 4:
+        cout << "CETVRTAK - ";
+    case 5:
+        cout << "PETAK - ";
+    case 6:
+        cout << "SUBOTA - ";
+    case 7:
+        cout << "NEDJELJA";
+        break;
+
+    default:
+        cout << "Pogresan unos!";
+    }
+
+    cout << endl;
+    return 0;  
+
+}
