f8e92a865cf656182b154d1eeffc73dafc22ff68
GabrielMurkovic <murkovicgabriel@gmail.com>
2026-06-06 18:21:04 +0200

    Add files via upload

---
+++ LV21 - Program koji provjerava nedostaju li zagrade.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+string bintodec(int broj) {
+	stack <int> s;
+	string binstring = "";
+	while(broj>0) {
+		int rem = broj%2;
+		s.push(rem);
+		broj = broj/2;
+
+	}
+
+	while(!s.empty()){
+	binstring.append(to_string(s.top()));
+	s.pop();
+	}
+	return binstring;
+}
+int main() {
+	cout << bintodec(42);
+}
+
