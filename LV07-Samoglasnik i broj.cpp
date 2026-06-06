853acf0056ef44d26420dd5c6ab18b548e01e019
GabrielMurkovic <murkovicgabriel@gmail.com>
2026-06-06 19:04:15 +0200

    Add files via upload

---
+++ LV07-Samoglasnik i broj.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+
+    char znak;
+
+    cout << "Unesite jedan znak: ";
+    cin >> znak;
+
+    switch (znak) {
+
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+    case '%':
+        cout << "OPERATOR" << endl;
+        break;
+
+    case 'A': case 'E': case 'I': case 'O': case 'U':
+    case 'a': case 'e': case 'i': case 'o': case 'u':
+        cout << "SAMOGLASNIK" << endl;
+        break;
+
+    case '.':
+    case ',':
+    case ';':
+    case ':':
+    case '?':
+    case '!':
+        cout << "INTERPUNKCIJA" << endl;
+        break;
+
+    default:
+        if (znak >= '0' && znak <= '9') {
+            cout << "ZNAMENKA" << endl;
+        }
+        else {
+            cout << "OSTALI ZNAKOVI" << endl;
+        }
+    }
+
+    return 0;
+}
